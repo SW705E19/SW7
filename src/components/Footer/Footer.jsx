535ea61b7fc69297a7dfe1030817d40e81e32012
jenspetur94 <jenspeturtrondarson@gmail.com>
2019-10-22 13:26:53 +0200

    actual initial work on footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Footer.module.css';
+import Grid from '@material-u'
+
+function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <Grid>
+
+      </Grid>
+    </footer>
+  );
+}
+
+export default Footer;
