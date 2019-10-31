2ee68ffe126347e824f8b645421b813ca3cc207e
unknown <jenspeturtrondarson@gmail.com>
2019-10-31 11:00:03 +0100

    fixed linter errors

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import LandingPage from '../../containers/LandingPage/LandingPage';
 import NotFound from '../../containers/NotFound/NotFound';
