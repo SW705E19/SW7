2fea1f4575118878a9fa04f1d04ddcc7867e11f1
Mathias <mathiaslybech@gmail.com>
2019-10-31 11:10:28 +0100

    header næsten færdig

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,7 +1,9 @@
 import  React from 'react';
-import { AppBar, Toolbar, IconButton, Hidden} from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Hidden, Button, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { AccountCircle , Notifications, Event, Mail} from '@material-ui/icons';
+import { AccountCircle, Menu, Notifications, Home, Event, Mail} from '@material-ui/icons';
+import Drawer from '@material-ui/core/Drawer';
+
 
 const styles = {
 	toolbarButtons: {
@@ -9,15 +11,82 @@ const styles = {
 	}
 };
 
-function Header({classes}) {
 
+function Header({classes}) {
+	const [state, setState] = React.useState({
+		left: false,
+	});
+
+	const toggleDrawer = (side, open) => event => {
+		if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+		  return;
+		}
+	
+		setState({ ...state, [side]: open });
+	};
+
+	const sideList = side => (
+		<div
+		  className={classes.list}
+		  role="presentation"
+		  onClick={toggleDrawer(side, false)}
+		  onKeyDown={toggleDrawer(side, false)}
+		>
+		  <List>
+		  <ListItem button key = 'Home'>
+				<ListItemText primary='Home' />
+				<ListItemIcon>
+					<Home fontSize="large" />
+				</ListItemIcon>
+			</ListItem>
+
+			<ListItem button key = 'Mail'>
+				<ListItemText primary='Inbox' />
+				<ListItemIcon>
+					<Mail fontSize="large" />
+				</ListItemIcon>
+			</ListItem>
+
+			<ListItem button key = 'Event'>
+				<ListItemText primary='Calendar' />
+				<ListItemIcon>
+					<Event fontSize="large" />
+				</ListItemIcon>
+			</ListItem>
+
+			<ListItem button key = 'Notifications'>
+				<ListItemText primary='Notifications' />
+				<ListItemIcon>
+					<Notifications fontSize="large" />
+				</ListItemIcon>
+			</ListItem>
+			
+			<ListItem button key = 'AccountCircle'>
+				<ListItemText primary='My account' />
+				<ListItemIcon>
+					<AccountCircle fontSize="large" />
+				</ListItemIcon>
+			</ListItem>
+		  </List>
+		</div>
+	  );
+	
+
+	
 	return (
 		<AppBar position="static" >
 			<Toolbar>
 				<Hidden mdUp>
-
+					<Button onClick={toggleDrawer('left', true)}><Menu/></Button>
+					<Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+   			     	   {sideList('left')}
+  			 	    </Drawer> 
 				</Hidden>
 				<Hidden smDown>
+					<IconButton className={classes.menuButton}>
+						<Home fontSize="large" />
+					</IconButton>
+
 					<div className={classes.toolbarButtons}>
 						<IconButton>
 							<Mail fontSize="large" />
