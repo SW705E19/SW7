6b9f7dbb008747c88e73e073d2fc3d74932df6e9
jenspetur94 <jenspeturtrondarson@gmail.com>
2019-10-31 13:26:58 +0100

    set up routing (#15)
    * set up routing
    
    * fixed linter errors
    
    * fixed some requested changes
    
    * added automatic translation to error page
    
    * Apply suggestions from code review
    
    Co-Authored-By: Daniel Moesgaard Andersen <moesgaarda@gmail.com>
    
    * removed unecessary div

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import NotFound from '../../containers/NotFound/NotFound';
 import Login from '../../containers/Login/Login';
 
 function Layout() {
+	const routing = (
+		<Router>
+			<Switch>
+				<Route path="/login" component={Login} />
+				<Route component={NotFound} />
+			</Switch>
+		</Router>
+	);
+
 	return (
 		<>
 			<div>Toolbar</div>
 			<div>Sidedrawer</div>
 			<div>Backdrop</div>
-			<main>
-				<Login />
-			</main>
+			<div>
+				{routing}
+			</div>
+
 			<div>Footer</div>
 		</>
 	);
