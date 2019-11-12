0259e10154ebc17f2b9b4f8aae57cb7d2013d6b3
Unknown <mathiaslybech@gmail.com>
2019-11-12 09:36:11 +0100

    routing tilføjet

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import NotFound from '../../containers/NotFound/NotFound';
-import Login from '../../containers/Login/Login';
-import Header from '../../components/Header/Header';
-
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import NotFound from "../../containers/NotFound/NotFound";
+import Login from "../../containers/Login/Login";
+import Header from "../../components/Header/Header";
+import CreateUser from "../../components/CreateUser/CreateUser";
 function Layout() {
-	const routing = (
-		<Router>
-			<Switch>
-				<Route path="/login" component={Login} />
-				<Route component={NotFound} />
-			</Switch>
-		</Router>
-	);
+  const routing = (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/createuser" component={CreateUser} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 
-	return (
-		<>
-			<Header />
-			<div>Sidedrawer</div>
-			<div>Backdrop</div>
-			<div>
-				{routing}
-			</div>
+  return (
+    <>
+      <Header />
+      <CreateUser />
+      <div>Sidedrawer</div>
+      <div>Backdrop</div>
+      <div>{routing}</div>
 
-			<div>Footer</div>
-		</>
-	);
+      <div>Footer</div>
+    </>
+  );
 }
 
 export default Layout;
