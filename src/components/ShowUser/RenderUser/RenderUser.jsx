b56702882b8c904b3618808631ac247f73ae6d21
Andreas Stenshøj <and8700and@gmail.com>
2019-11-12 15:57:33 +0100

    translations + eslint stuff

--- src/components/ShowUser/RenderUser/RenderUser.jsx
+++ src/components/ShowUser/RenderUser/RenderUser.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Avatar, Grid, Typography, CardMedia, CardContent } from '@material-ui/core';
+import { Card, Avatar, Grid, Typography, CardContent } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
 
 function RenderUser(props) {
 	const useStyles = makeStyles(theme => ({
@@ -16,6 +17,7 @@ function RenderUser(props) {
 		}
 	}));
 
+	const { t } = useTranslation();
 	const classes = useStyles();
 
 	const random = 1 + (Math.random() * (200 - 1));
@@ -26,53 +28,53 @@ function RenderUser(props) {
 		<>
 			<Card className={classes.card}>
 				<Grid container justify="center" alignItems="center">
-				<Avatar src={randomUrl} className={classes.avatar} />
+					<Avatar src={randomUrl} className={classes.avatar} />
 				</Grid>
 				<CardContent>
 					<Typography component="h2" variant="h5">
 						{props.user.firstName} {props.user.lastName}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
-						Email
+						{t('email')}
 					</Typography>
 					<Typography variant="caption" color="textSecondary">
 						{props.user.email}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
-						Date of Birth
-						</Typography>
+						{t('dateOfBirth')}
+					</Typography>
 					<Typography variant="caption" color="textSecondary">
 						{props.user.dateOfBirth}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
-						Languages
-						</Typography>
+						{t('languages')}
+					</Typography>
 					<Typography variant="caption" color="textSecondary">
 						{props.user.languages}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
-						Education
-						</Typography>
+						{t('education')}
+					</Typography>
 					<Typography variant="caption" color="textSecondary">
 						{props.user.education}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
-						Subjects
-						</Typography>
+						{t('subjectofinterst')}
+					</Typography>
 					<Typography variant="caption" color="textSecondary">
 						{props.user.subjects}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
-						Description
-						</Typography>
+						{t('description')}
+					</Typography>
 					<Typography variant="caption" color="textSecondary">
 						{props.user.description}
 					</Typography>
 				</CardContent>
 
-				{ props.user.roles != null && props.user.roles.includes("TUTOR") ? <CardContent>
+				{ props.user.roles != null && props.user.roles.includes('TUTOR') ? <CardContent>
 					<Typography component="h2" variant="h5">
-						Tutor info
+						{t('tutorInfo')}
 					</Typography>
 					<Typography variant="subtitle1" color="textPrimary">
 						{props.user.tutorInfo.description}
--- src/components/ShowUser/ShowUser.jsx
+++ src/components/ShowUser/ShowUser.jsx
@@ -6,16 +6,16 @@ class ShowUser extends Component {
 		super(props);
 		this.state = {
 			user: []
-		}
+		};
 	}
 
 	componentDidMount() {
 		fetch(`http://${process.env.REACT_APP_API_URI}:${process.env.REACT_APP_API_PORT}/api/users/${this.props.match.params.id}`)
-		.then(res => res.json())
-		.then((data) => {
-			this.setState({ user: data })
-		})
-		.catch(console.log)
+			.then(res => res.json())
+			.then((data) => {
+				this.setState({ user: data });
+			})
+			.catch(console.log);
 	}
 	render() {
 		return (<RenderUser user={this.state.user} />);
