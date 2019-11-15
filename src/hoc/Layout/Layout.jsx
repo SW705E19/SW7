586407767f0ba430e76b2f2b5646aa00adba6a2e
Unknown <mathiaslybech@gmail.com>
2019-11-15 09:20:52 +0100

    Auto stash before merge of "create-user" and "origin/create-user"

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import { Container } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import NotFound from '../../containers/NotFound/NotFound';
-import Login from '../../containers/Login/Login';
-import ShowUser from '../../components/ShowUser/ShowUser';
-import Header from '../../components/Header/Header';
-import { UserForm } from '../../components/CreateUser/UserForm';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import NotFound from "../../containers/NotFound/NotFound";
+import Login from "../../containers/Login/Login";
+import ShowUser from "../../components/ShowUser/ShowUser";
+import Header from "../../components/Header/Header";
+import { UserForm } from "../../components/CreateUser/UserForm";
 
 function Layout() {
-	const routing = (
-		<Router>
-			<Switch>
-				<Route path="/login" component={Login} />
-				<Route path="/user/:id" component={ShowUser} />
-				<Route path="/createuser" component={UserForm} />
-				<Route component={NotFound} />
-			</Switch>
-		</Router>
-	);
+  const routing = (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/user/:id" component={ShowUser} />
+        <Route path="/createuser" component={UserForm} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 
-	const useStyles = makeStyles(theme => ({
-		appBarSpacer: theme.mixins.toolbar,
-	}));
+  const useStyles = makeStyles(theme => ({
+    appBarSpacer: theme.mixins.toolbar
+  }));
 
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<>
-			<Header />
-			<div className={classes.appBarSpacer} />
-			<Container component="main" maxWidth="sm" p={8}>
-				{routing}
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <UserForm />
+      <div className={classes.appBarSpacer} />
+      <Container component="main" maxWidth="sm" p={8}>
+        {routing}
+      </Container>
+    </>
+  );
 }
 
 export default Layout;
