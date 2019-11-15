006929ab31c6409d4f4cff09f2933e58e3d70525
andpaacoke <and8700and@gmail.com>
2019-11-15 12:56:32 +0100

    Update AdminDashboard.jsx

--- src/containers/AdminDashboard/AdminDashboard.jsx
+++ src/containers/AdminDashboard/AdminDashboard.jsx
@@ -2,14 +2,9 @@ import React, { Component } from 'react';
 import RenderCategories from './Category/RenderCategories';
 
 class AdminDashboard extends Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
 		return (
-			<>
-				<RenderCategories ></RenderCategories>
-			</>
+			<RenderCategories ></RenderCategories>
 		);
 	}
 }
