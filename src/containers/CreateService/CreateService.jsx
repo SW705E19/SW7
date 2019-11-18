86a12262171df7a7d1ffe1f0cfddeb4cd58da13a
Andreas Stenshøj <and8700and@gmail.com>
2019-11-18 15:11:52 +0100

    stuff

--- src/containers/CreateService/CreateService.jsx
+++ src/containers/CreateService/CreateService.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import { categoryService } from '../../services/category/category.service';
-import { Container, Select, MenuItem } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { Container, Select, MenuItem, Button, Grid, Typography, FormControl, TextField, InputLabel} from '@material-ui/core';
+import { withTranslation } from 'react-i18next';
+
+
+const styles = theme => ({
+	formControl: {
+	  margin: theme.spacing(1),
+	  minWidth: '30%',
+	  maxWidth: '80%',
+	},
+});
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250,
+		},
+	},
+};
 
 class CreateService extends React.Component {
 	constructor(props) {
@@ -13,12 +35,10 @@ class CreateService extends React.Component {
 				tutorInfo: {},
 				categories: []
 			},
-
-			categories: []
+			categories: [],
 		};
         
 		this.handleOnChange = this.handleOnChange.bind(this);
-		this.handleCategoriesChange = this.handleCategoriesChange.bind(this);
 	}
     
 	componentDidMount() {
@@ -33,39 +53,91 @@ class CreateService extends React.Component {
 		this.setState({service: service});
 	}
     
-	handleCategoriesChange(event) {
-		let service = this.state.service;
-		service.categories.push(event);
-		this.setState({service: service});
-	}
-    
 	menuItems(suggestions) {
-		return suggestions.map((value, i) => (
-			<MenuItem key={i} value={value.name}>
+		return suggestions.map((value) => (
+			<MenuItem key={value.id} value={value.name} >
 				{value.name}
 			</MenuItem>
 		));
 	}
+
     
 	render() {
+		const{classes, t} = this.props;
 		return(
 			<> 
 				<Container maxWidth="sm">
-					<Select
-						fullWidth
-						name="categories"
-						value={this.state.service.categories}
-						onChange={this.handleCategoriesChange}
-						multiple={true}
-						renderValue={selected => selected.name}
-						variant="outlined">
-						{this.menuItems(this.state.categories)}
-					</Select>
+					<Typography >
+						Create a service
+					</Typography>
+					<FormControl  noValidate autoComplete="off" className={classes.formControl}>
+						<Grid container spacing={2} direction="row">
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									margin="normal"
+									required
+									fullWidth
+									id={'name'}
+									label={('Name')}
+									name={'name'}
+									autoComplete={'name'}
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									id="outlined-textarea"
+									label="Description"
+									multiline
+									fullWidth
+									margin="normal"
+									variant="outlined"
+								/>
+							</Grid>
+							
+
+							<Grid item xs={12}>
+								<InputLabel id="categories">Categories</InputLabel>
+								<Select 
+									fullWidth
+									labelId="categories"
+									value={this.state.service.categories}
+									onChange={this.handleOnChange}
+									name="categories"
+									variant="outlined"
+									renderValue={selected => {
+										if(selected.length === 0) {
+											return null; }
+										else {
+											return selected.map(cat => {
+												return cat;
+											}).join(', ');
+
+											
+										}
+									}}
+									MenuProps={MenuProps}
+									multiple={true}>
+									{this.menuItems(this.state.categories)}
+								</Select>
+							</Grid>
+							
+							<Grid item xs={12}>
+								<Button type="submit" fullWidth variant="contained" color="inherit">
+								Save
+								</Button>
+							</Grid>
+
+						</Grid>
+					</FormControl>
 				</Container>
+
+				
 			</>
 		);
 
 	}
 }
 
-export default CreateService;
+export default withTranslation()(withStyles(styles)(CreateService));
