5f7d2c06fd57cbd60d1145351eda3a53970edd2d
Rasmus Eduardsen <eduardsengithub@gmail.com>
2019-11-26 11:33:41 +0100

    updated render tests.

--- src/components/ShowService/Service.test.jsx
+++ src/components/ShowService/Service.test.jsx
@@ -5,16 +5,34 @@ import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
 import RenderService from './RenderService/RenderService';
-import { Button, Grid, Card, CardContent, CardActions, Typography, Avatar, CardMedia } from '@material-ui/core';
+import { Button, Grid, Card, CardContent, CardActions, Typography, Avatar, CardMedia, Divider, Box } from '@material-ui/core';
 
 configure({adapter: new Adapter()});
 
-describe('<Login />', () => {
+describe('<RenderService />', () => {
 	let wrapper;
 	let testinstance;
+	let tutor = {
+		firstName: 'Per',
+		lastName: 'Hansen',
+	};
+	let tutorInfo = {
+		description: 'I am a tutor',
+	};
+	let service = {
+		name: 'Service name',
+		description: 'And this is my service',
+	};
+	let category = [
+		{
+			name: 'Category1',
+		}, {
+			name: 'Category2',
+		}
+	];
 
 	beforeEach(() => {
-		wrapper = renderer.create(<RenderService />);
+		wrapper = renderer.create(<RenderService service={service} tutor={tutor} tutorInfo={tutorInfo} categories ={category}/>);
 		testinstance  = wrapper.root;
 	});
 
@@ -48,5 +66,13 @@ describe('<Login />', () => {
 
 	it('should render <CardMedia />', () => {
 		expect(testinstance.findAllByType(CardMedia)).toHaveLength(1);
+	});
+
+	it('should render all <Divider />', () => {
+		expect(testinstance.findAllByType(Divider)).toHaveLength(3);
+	});
+
+	it('should render all <Box />', () => {
+		expect(testinstance.findAllByType(Box)).toHaveLength(3);
 	});
 });
