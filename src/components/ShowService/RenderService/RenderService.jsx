3d24113e70243bb163d75b1b0b0edeb00352fa7f
Rasmus Eduardsen <eduardsengithub@gmail.com>
2019-12-16 13:53:37 +0100

    bruh, fixing moment

--- src/components/ShowService/RenderService/RenderService.jsx
+++ src/components/ShowService/RenderService/RenderService.jsx
@@ -123,7 +123,7 @@ function RenderService(props) {
 								<Divider orientation="horizontal" />
 								<Box fontStyle="italic" >
 									<Typography color="textPrimary" style={{ fontSize: 12 }}>
-										{'Categories: ' + props.service.categories.map((category, index) => category.name + (index == props.service.categories.length-1 ? '.': '')).join(', ')}
+										{'Categories: ' + props.service.categories.map((category, index) => category.name + (props.service.categories.length-1 === index ? '.' : ', ')).join('')}
 									</Typography>
 								</Box>
 								<Divider orientation="horizontal" />
@@ -177,7 +177,7 @@ function RenderService(props) {
 							<Divider orientation="horizontal" />
 							<Box fontStyle="italic" >
 								<Typography color="textPrimary" style={{ fontSize: 12 }}>
-									{'Categories: ' + props.service.categories.map((category, index) => category.name + (index ? '.' : ', ')).join('')}
+									{'Categories: ' + props.service.categories.map((category, index) => category.name + (props.service.categories.length-1 === index ? '.' : ', ')).join('')}
 								</Typography>
 							</Box>
 							<Divider orientation="horizontal" />
