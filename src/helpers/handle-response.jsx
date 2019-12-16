3da3518c7e51638e02814c4f2776b3a358c13276
Unknown <and8700and@gmail.com>
2019-12-16 11:45:45 +0100

    Fjernet så man ikke blive logget ud ved fejl response

--- src/helpers/handle-response.jsx
+++ src/helpers/handle-response.jsx
@@ -6,10 +6,6 @@ export default function handleResponse(response) {
 	return response.text().then(text => {
 		const data = text;
 		if (!response.ok) {
-			if ([400, 401, 403].indexOf(response.status) !== -1) {
-				authenticationService.logout();
-			}
-
 			const error = (data && data.message) || response.statusText;
 			return Promise.reject(error);
 		}
